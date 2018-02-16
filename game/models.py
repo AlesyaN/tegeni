bfff42225fdc9befefafd78f63704ea6f9cc6995
Timur Rylov <rtimur1999@gmail.com>
2018-02-16 12:50:24 +0300

    added in admin panel levels

--- game/models.py
+++ game/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Levels(models.Model):
+    name  = models.TextField()
+    img = models.ImageField(upload_to='game',
+                      height_field=100, width_field=100)
+    val = models.CharField(max_length = 6);
+
+def __str__(self):
+    return self.title
