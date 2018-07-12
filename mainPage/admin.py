edf28406a1adfde317d6433ba374e2f2df01d11c
Timur Rylov <rtimur1999@gmail.com>
2018-07-12 17:20:46 +0300

    d

--- mainPage/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from game.models import Levels
-
-admin.site.register(Levels)
-# Register your models here.
--- mainPage/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def HelloPage(request):
-    return render(request , "mainPage/index.html" )
